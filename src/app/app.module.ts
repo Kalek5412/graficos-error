2573173fd01fc7b8b6ba6630f6911c4737728aad
Informatica02 <Informatica02@MATERNIDAD.LOCAL>
2023-01-14 10:55:08 -0500

    grficos aun con error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { FotterComponent } from './fotter/fotter.component';
+import { PieComponent } from './graficos/pie/pie.component';
+import { DonutComponent } from './graficos/donut/donut.component';
+import { BarComponent } from './graficos/bar/bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { esLocale } from 'ngx-bootstrap/locale';
+defineLocale('es', esLocale);
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    BottomComponent,
+    FotterComponent,
+    PieComponent,
+    DonutComponent,
+    BarComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  //ng2-chart///////////
+  title = 'ng2-charts-demo';
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false
+  };
+  public lineChartLegend = true;
+
+  /////////////////////////////////////
+  locale='es'
+  constructor(private localeService: BsLocaleService) { 
+    this.localeService.use(this.locale)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
